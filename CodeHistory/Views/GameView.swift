582f6181d1709d0e7531058ad45fe81e6201d02a
Ben Stone <benjamin.h.stone@gmail.com>
2021-03-04 18:19:28 -0500

    add the welcome screen, bottom text view, and gamecolor

--- CodeHistory/Views/GameView.swift
+++ CodeHistory/Views/GameView.swift
@@ -7,28 +7,29 @@
 
 import SwiftUI
 
-struct ContentView: View {
+struct GameView: View {
     
     let question = Question(
         questionText: "What was the first computer bug?",
         possibleAnswers: ["Ant", "Beetle", "Moth", "Fly"],
         correctAnswerIndex: 2)
     
-    @State var mainColor = Color(red: 20/255, green: 28/255, blue: 58/255)
-    let accentColor = Color(red: 48/255, green: 105/255, blue: 240/255)
+    @State var mainColor = GameColor.main
     
     var body: some View {
         ZStack {
             mainColor.ignoresSafeArea()
             VStack {
-                Text("1 / 10")
-                    .font(.callout)
-                    .multilineTextAlignment(.leading)
-                    .padding()
-                Text(question.questionText)
-                    .font(.largeTitle)
-                    .bold()
-                    .multilineTextAlignment(.leading)
+                VStack {
+                    Text("1 / 10")
+                        .font(.callout)
+                        .multilineTextAlignment(.leading)
+                        .padding()
+                    Text(question.questionText)
+                        .font(.largeTitle)
+                        .bold()
+                        .multilineTextAlignment(.leading)
+                }
                 Spacer()
                 HStack {
                     ForEach(0..<question.possibleAnswers.count) { answerIndex in
@@ -36,13 +37,14 @@ struct ContentView: View {
                             print("Tapped on option with the text: \(question.possibleAnswers[answerIndex])")
                             
                             // The line below should be the last thing learners add in this article
-                            mainColor = answerIndex == question.correctAnswerIndex ? .green : .red
+                            mainColor = answerIndex == question.correctAnswerIndex ? GameColor.correctGuess : GameColor.incorrectGuess
                         }) {
                             ChoiceTextView(choiceText: question.possibleAnswers[answerIndex])
                         }
                     }
                 }
             }
+            
         }
         .foregroundColor(.white)
     }
@@ -51,6 +53,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        ContentView()
+        GameView()
     }
 }
