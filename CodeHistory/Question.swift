b4cce2be05520f876edd41ac858aa6837438e806
Ben Stone <benjamin.h.stone@gmail.com>
2021-03-04 17:46:27 -0500

    add question and choice text view

---
+++ CodeHistory/Question.swift
@@ -0,0 +1,14 @@
+//
+//  Question.swift
+//  CodeHistory
+//
+//  Created by Ben Stone on 3/4/21.
+//
+
+import Foundation
+
+struct Question {
+    let questionText: String
+    let possibleAnswers: [String]
+    let correctAnswerIndex: Int
+}
